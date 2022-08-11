dd8635b2669d720472c604e781ac709e3a2ae608
Vamshikiran Morlawar <vamshikiranmorlawar@Vamshikirans-MacBook-Air.local>
2022-08-11 15:16:16 +0530

    Correction and Used the fifth satellite data as well to precise the user location

--- Assignment_1_22111066/solution.py
+++ Assignment_1_22111066/solution.py
@@ -38,13 +38,15 @@ print(
 A = np.array([
     [2*(satellite_2_loc[0] - satellite_1_loc[0]), 2*(satellite_2_loc[1] - satellite_1_loc[1]), 2*(satellite_2_loc[2] - satellite_1_loc[2])],
     [2*(satellite_3_loc[0] - satellite_2_loc[0]), 2*(satellite_3_loc[1] - satellite_2_loc[1]), 2*(satellite_3_loc[2] - satellite_2_loc[2])],
-    [2*(satellite_4_loc[0] - satellite_3_loc[0]), 2*(satellite_4_loc[1] - satellite_3_loc[1]), 2*(satellite_4_loc[2] - satellite_3_loc[2])]
+    [2*(satellite_4_loc[0] - satellite_3_loc[0]), 2*(satellite_4_loc[1] - satellite_3_loc[1]), 2*(satellite_4_loc[2] - satellite_3_loc[2])],
+    [2*(satellite_5_loc[0] - satellite_4_loc[0]), 2*(satellite_5_loc[1] - satellite_4_loc[1]), 2*(satellite_5_loc[2] - satellite_4_loc[2])]
 ])
 
 b = np.array([
     [(distance_u_1**2 - distance_u_2**2) - (satellite_1_loc[0]**2 - satellite_2_loc[0]**2) - (satellite_1_loc[1]**2 - satellite_2_loc[1]**2) - (satellite_1_loc[2]**2 - satellite_2_loc[2]**2)],
     [(distance_u_2**2 - distance_u_3**2) - (satellite_2_loc[0]**2 - satellite_3_loc[0]**2) - (satellite_2_loc[1]**2 - satellite_3_loc[1]**2) - (satellite_2_loc[2]**2 - satellite_3_loc[2]**2)],
-    [(distance_u_3**2 - distance_u_4**2) - (satellite_3_loc[0]**2 - satellite_4_loc[0]**2) - (satellite_3_loc[1]**2 - satellite_4_loc[1]**2) - (satellite_3_loc[2]**2 - satellite_4_loc[2]**2)]
+    [(distance_u_3**2 - distance_u_4**2) - (satellite_3_loc[0]**2 - satellite_4_loc[0]**2) - (satellite_3_loc[1]**2 - satellite_4_loc[1]**2) - (satellite_3_loc[2]**2 - satellite_4_loc[2]**2)],
+    [(distance_u_4**2 - distance_u_5**2) - (satellite_4_loc[0]**2 - satellite_5_loc[0]**2) - (satellite_4_loc[1]**2 - satellite_5_loc[1]**2) - (satellite_4_loc[2]**2 - satellite_5_loc[2]**2)]
 ])
 
 x1 = np.linalg.inv((np.dot(A.transpose(), A))) 
@@ -62,23 +64,25 @@ print(
 #Using the times calculated in (a) and the satellite locations we get the user location very close to the considered location
 
 
-def distanceError(rand_time_error):
-    distance_1 = distance_u_1 - rand_time_error * speed_of_light
-    distance_2 = distance_u_2 - rand_time_error * speed_of_light
-    distance_3 = distance_u_3 - rand_time_error * speed_of_light
-    distance_4 = distance_u_4 - rand_time_error * speed_of_light
-    distance_5 = distance_u_5 - rand_time_error * speed_of_light #fifth satellite data is not being used as we ourself are considering some random time error
+def localizationError(rand_time_error):
+    distance_1 = distance_u_1 + rand_time_error * speed_of_light
+    distance_2 = distance_u_2 + rand_time_error * speed_of_light
+    distance_3 = distance_u_3 + rand_time_error * speed_of_light
+    distance_4 = distance_u_4 + rand_time_error * speed_of_light
+    distance_5 = distance_u_5 + rand_time_error * speed_of_light #fifth satellite data is not being used as we ourself are considering some random time error
 
     A = np.array([
         [2*(satellite_2_loc[0] - satellite_1_loc[0]), 2*(satellite_2_loc[1] - satellite_1_loc[1]), 2*(satellite_2_loc[2] - satellite_1_loc[2])],
         [2*(satellite_3_loc[0] - satellite_2_loc[0]), 2*(satellite_3_loc[1] - satellite_2_loc[1]), 2*(satellite_3_loc[2] - satellite_2_loc[2])],
-        [2*(satellite_4_loc[0] - satellite_3_loc[0]), 2*(satellite_4_loc[1] - satellite_3_loc[1]), 2*(satellite_4_loc[2] - satellite_3_loc[2])]
+        [2*(satellite_4_loc[0] - satellite_3_loc[0]), 2*(satellite_4_loc[1] - satellite_3_loc[1]), 2*(satellite_4_loc[2] - satellite_3_loc[2])],
+        [2*(satellite_5_loc[0] - satellite_4_loc[0]), 2*(satellite_5_loc[1] - satellite_4_loc[1]), 2*(satellite_5_loc[2] - satellite_4_loc[2])]
     ])
 
     b = np.array([
         [(distance_1**2 - distance_2**2) - (satellite_1_loc[0]**2 - satellite_2_loc[0]**2) - (satellite_1_loc[1]**2 - satellite_2_loc[1]**2) - (satellite_1_loc[2]**2 - satellite_2_loc[2]**2)],
         [(distance_2**2 - distance_3**2) - (satellite_2_loc[0]**2 - satellite_3_loc[0]**2) - (satellite_2_loc[1]**2 - satellite_3_loc[1]**2) - (satellite_2_loc[2]**2 - satellite_3_loc[2]**2)],
-        [(distance_3**2 - distance_4**2) - (satellite_3_loc[0]**2 - satellite_4_loc[0]**2) - (satellite_3_loc[1]**2 - satellite_4_loc[1]**2) - (satellite_3_loc[2]**2 - satellite_4_loc[2]**2)]
+        [(distance_3**2 - distance_4**2) - (satellite_3_loc[0]**2 - satellite_4_loc[0]**2) - (satellite_3_loc[1]**2 - satellite_4_loc[1]**2) - (satellite_3_loc[2]**2 - satellite_4_loc[2]**2)],
+        [(distance_4**2 - distance_5**2) - (satellite_4_loc[0]**2 - satellite_5_loc[0]**2) - (satellite_4_loc[1]**2 - satellite_5_loc[1]**2) - (satellite_4_loc[2]**2 - satellite_5_loc[2]**2)]
     ])
 
     x1 = np.linalg.inv((np.dot(A.transpose(), A))) 
@@ -86,31 +90,34 @@ def distanceError(rand_time_error):
     user_loc_calculated = np.dot(x1, x2)
 
     new_user_loc_calculated = np.array([user_loc_calculated[0][0], user_loc_calculated[1][0], user_loc_calculated[2][0]])
-    distance_error = abs(np.linalg.norm(new_user_loc_calculated - user_loc))
-    return distance_error
+    localization_error = abs(np.linalg.norm(new_user_loc_calculated - user_loc))
+    return localization_error
 
-rand_time_error = np.random.random()
-distance_error = distanceError(rand_time_error)
-print('\n(c) Check how much location inaccuracy it showing up?\ntime error - ', rand_time_error, 's, location inaccuracy - ', distance_error)
+rand_time_error = np.random.uniform(pow(10,-9), 99*pow(10,-9))
+localization_error = localizationError(rand_time_error)
+print('\n(c) Check how much location inaccuracy it showing up?\ntime error - ', rand_time_error, 's, location inaccuracy - ', localization_error)
 #the distance inaccuracy is too high when the time error is high like in seconds instead of ns, micro seconds
 
-distance_error_list = []
+localization_error_list = []
+time_error_list = []
 # time_error_list = [pow(10, -9), pow(10, -6), pow(10, -3), 1]
-time_error_list = np.random.rand(100)
+for i in range(100):
+    time_error_list.append(np.random.uniform(pow(10,-9), 99*pow(10,-9)))
 
 for time_error in time_error_list:
-    distance_error_list.append(distanceError(time_error))
+    localization_error_list.append(localizationError(time_error))
 
 #d
-average_localization_error = sum(distance_error_list)/len(distance_error_list)
+average_localization_error = sum(localization_error_list)/len(localization_error_list)
 print('\n(d) Take the average localization errors = ', average_localization_error)
 
 plt.figure(num=0, dpi=120)
 plt.title('(d) timing errors vs localization error')
-plt.xlabel('Time Errors')
-plt.ylabel('Localization Errors')
+plt.xlabel('Time Errors in (Seconds)')
+plt.ylabel('Localization Errors in (Meters)')
 
-plt.plot(time_error_list, distance_error_list)
+plt.plot(time_error_list, localization_error_list)
+plt.scatter(time_error_list, localization_error_list, c='purple')
 plt.show()
 
 #the plot of the time error and localisation error plot increases with increasing time error
